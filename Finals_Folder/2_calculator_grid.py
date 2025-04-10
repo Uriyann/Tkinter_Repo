77a660905e05e098237b582c8c1e789f840e7563
Uriyann <joshuabarotea06@gmail.com>
2025-04-10 16:49:01 +0800

    basic

--- Finals_Folder/2_calculator_grid.py
+++ Finals_Folder/2_calculator_grid.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Login Form")
+window.title("Calc")
 window.geometry('400x400')
 
+frame = tk.Frame()
 
+num_entry = tk.Entry(frame)
+ans_entry = tk.Entry(frame)
+
+num_entry.grid(row=0, column=0, rowspan=1, pady= 4)
+ans_entry.grid(row=1, column=0, rowspan=1, pady= 4)
+
+frame.pack()
 
 
 window.mainloop()
