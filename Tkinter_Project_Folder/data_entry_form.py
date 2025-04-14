9eeea62de229cabef8882b688e49d7971bd6bdf0
Uriyann <joshuabarotea06@gmail.com>
2025-04-14 22:40:35 +0800

    new

--- Tkinter_Project_Folder/data_entry_form.py
+++ Tkinter_Project_Folder/data_entry_form.py
@@ -12,8 +12,8 @@ frame = tk.Frame(window)
 frame.pack()
 
 # User Frame
-user_info_frame = tk.LabelFrame(frame, text="User Information", font= ("times new roman", 17))
-user_info_frame.grid(row=0, column=0, sticky= NSEW, pady=20, padx=20)
+user_info_frame = tk.LabelFrame(frame, text="User Information", font= ("times new roman", 15))
+user_info_frame.grid(row=0, column=0, sticky= NSEW, pady=10, padx=20)
 
 # Name Label and Entry
 first_name_label = tk.Label(user_info_frame, text= "First Name:", font= ("arial normal", 10))
@@ -39,7 +39,7 @@ last_name_entry.grid(row=2, column=1, pady=3)
 
 # Secondary Frame
 second_frame = tk.LabelFrame(frame)
-second_frame.grid(row=1, column=0, sticky= NSEW, pady=20, padx=20)
+second_frame.grid(row=1, column=0, sticky= NSEW, pady=10, padx=20)
 
 # Age
 age_label = tk.Label(second_frame, text= "Age:", font= ("arial normal", 10))
@@ -54,7 +54,7 @@ month_entry = ttk.Combobox(second_frame, values= [
                                                     ], state= "readonly", font= ("arial normal", 10), width=10)
 days = list(range(1, 31))
 day_entry = ttk.Combobox(second_frame, values= days, state= "readonly", font= ("arial normal", 10), width=5)
-years = list(range(2000, 2051))
+years = list(range(1950, 2051))
 year_entry = ttk.Combobox(second_frame, values= years, state= "readonly", font= ("arial normal", 10), width=5)
 
 # Gender
@@ -71,57 +71,79 @@ birthplace_entry = tk.Entry(second_frame, width=15)
 # Secondary Frame Info Grid
 
 # Age Grid
-age_label.grid(row=4, column=0, pady=5, padx=5, sticky= "e")
-age_entry.grid(row=4, column=1, pady=5, padx=5)
+age_label.grid(row=0, column=0, pady=5, padx=5, sticky= "e")
+age_entry.grid(row=0, column=1, pady=5, padx=5)
 
 # Birthdate Grid
-birthdate_label.grid(row=4, column=2, pady=5, padx=5, sticky= "e")
-month_entry.grid(row=4, column=3, pady=5, padx=2)
-day_entry.grid(row=4, column=4, pady=5, padx=2)
-year_entry.grid(row=4, column=5, pady=5, padx=2)
+birthdate_label.grid(row=0, column=2, pady=5, padx=5, sticky= "e")
+month_entry.grid(row=0, column=3, pady=5, padx=2)
+month_entry.set("MM")
+day_entry.grid(row=0, column=4, pady=5, padx=2)
+day_entry.set("dd")
+year_entry.grid(row=0, column=5, pady=5, padx=2)
+year_entry.set("YYYY")
 
 # Gender Grid
-gender_label.grid(row=5, column=0, pady=5, padx=5, sticky= "e")
-male_entry.grid(row=5, column=1, pady=5, padx=2)
-female_entry.grid(row=6, column=1, pady=5, padx=2)
+gender_label.grid(row=1, column=0, pady=5, padx=5, sticky= "e")
+male_entry.grid(row=1, column=1, pady=5, padx=2)
+female_entry.grid(row=2, column=1, pady=5, padx=2)
 
 # Birthplace Grid
-birthplace_label.grid(row=5, column=2, pady=5, padx=5, sticky= "e")
-birthplace_entry.grid(row=5, column=3, pady=5, padx=2)
+birthplace_label.grid(row=1, column=2, pady=5, padx=5, sticky= "e")
+birthplace_entry.grid(row=1, column=3, pady=5, padx=2)
 
 
 # Third Frame
 third_frame = tk.LabelFrame(frame)
-third_frame.grid(row=2, column=0, sticky= NSEW, pady=20, padx=20)
+third_frame.grid(row=2, column=0, sticky= NSEW, pady=10, padx=20)
 
 # Nationality
 nationality_label = tk.Label(third_frame, text= "Nationality:", font= ("arial normal", 10))
-nationality_entry = ttk.Combobox(third_frame, values= ["Filipino", "Japanese", "American", "Chinese"], state= "readonly", font= ("arial normal", 10), width=15)
+nationality_entry = ttk.Combobox(third_frame, values= ["Filipino", "American", "British", "Chinese", "Indian", "Japanese", "Korean", 
+                                                       "German", "French", "Italian", "Canadian", "Australian", "Spanish", "Mexican",
+                                                       "Brazilian", "Russian", "Indonesian", "Vietnamese", "Thai", "Saudi Arabian",
+                                                       "South African", "Malaysian", "Singaporean", "Other"], 
+                                                       state= "readonly", font= ("arial normal", 10))
 
 # Marital Status
 marital_label = tk.Label(third_frame, text= "Marital Status:", font= ("arial normal", 10))
-marital_entry = ttk.Combobox(third_frame, values= ["Single", "Married", "Divorced", "Widowed"], state= "readonly", font= ("arial normal", 10), width=10)
+marital_entry = ttk.Combobox(third_frame, values= ["Single", "Married", "Divorced", "Widowed"], state= "readonly", font= ("arial normal", 10))
 
 # Religion
 religion_label = tk.Label(third_frame, text= "Religion:", font= ("arial normal", 10))
-religion_entry = ttk.Combobox(third_frame, values= ["Roman Catholic", "Iglesia", "Muslim"], state= "readonly", font= ("arial normal", 10), width=15)
+religion_entry = ttk.Combobox(third_frame, values= ["Christianity", "Islam", "Hinduism", "Buddhism", "Judaism", "Sikhism", "Atheism",
+                                                    "Agnosticism", "Taoism", "Shinto", "Bahá'í Faith", "Jainism", "Paganism", "Other"], 
+                                                    state= "readonly", font= ("arial normal", 10))
 
+language_label = tk.Label(third_frame, text= "Language:", font= ("arial normal", 10))
+language_entry = ttk.Combobox(third_frame, values= ["English", "Filipino", "Mandarin Chinese", "Spanish", "Hindi", "Arabic",
+                                                    "Bengali", "Portuguese", "Russian", "Japanese", "French", "German", "Korean",
+                                                    "Italian", "Vietnamese", "Turkish", "Persian (Farsi)", "Swahili", "Thai", "Malay/Indonesian"], 
+                                                    state= "readonly", font= ("arial normal", 10))
 
 # Third Frame Info Grid
 
 # Nationality Grid
-nationality_label.grid(row=8, column=0, pady=5, padx=5, sticky= "e")
-nationality_entry.grid(row=8, column=1, pady=5, padx=2)
+nationality_label.grid(row=0, column=0, pady=5, padx=5, sticky= "e")
+nationality_entry.grid(row=0, column=1, pady=5, padx=2)
+nationality_entry.set("Select Nationality")
 
 # Marital Status Grid
-marital_label.grid(row=8, column=2, pady=5, padx=5, sticky= "e")
-marital_entry.grid(row=8, column=3, pady=3, padx=3)
+marital_label.grid(row=0, column=2, pady=5, padx=5, sticky= "e")
+marital_entry.grid(row=0, column=3, pady=3, padx=3)
+marital_entry.set("Select Marital Status")
 
 # Religion Grid
-religion_label.grid(row=9, column=0, pady=5, padx=5, sticky= "e")
-religion_entry.grid(row=9, column=1, pady=5, padx=2)
+religion_label.grid(row=1, column=0, pady=5, padx=5, sticky= "e")
+religion_entry.grid(row=1, column=1, pady=5, padx=2)
+religion_entry.set("Select Religion")
 
+# Language Grid
+language_label.grid(row=1, column=2, pady=5, padx=5, sticky= "e")
+language_entry.grid(row=1, column=3, pady=5, padx=2)
+language_entry.set("Select Language")
 
+# Fourth Frame
 
 
 
